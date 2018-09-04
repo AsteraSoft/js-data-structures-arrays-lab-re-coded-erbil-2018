d339e89521dbbd02d72669f502bf3292f059a093
astera2.muhamad@gmail.com Muhammad Noori <astera2.muhamad@gmail.com>
2018-09-04 20:58:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
 // Write your solution here!
-const drivers=["Milo", "Otis", "Garfield"];
- function destructivelyAppendDriver(name){
+
+const drivers = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendDriver (name) {
   drivers.push(name);
 }
-function destructivelyPrependDriver(name)
-{
+
+function destructivelyPrependDriver (name) {
   drivers.unshift(name);
 }
 
-function destructivelyPrependDriver(){
-    drivers.pop();
+function destructivelyRemoveLastDriver () {
+  drivers.pop();
+}
 
+function destructivelyRemoveFirstDriver () {
+  drivers.shift();
+}
+
+function appendDriver (name) {
+  return [...drivers, name];
+}
+
+function prependDriver (name) {
+  return [name, ...drivers];
+}
+
+function removeFirstDriver () {
+  return drivers.slice(1);
+}
+
+function removeLastDriver () {
+  return drivers.slice(0, drivers.length - 1);
 }
